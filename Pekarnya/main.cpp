6987e001d6a43aafec851f6d9c6b6542c348e747
Roman Ivanov <marsianinszemli@gmail.com>
2023-12-10 13:53:40 +0300

    add constructor for worker

--- Pekarnya/main.cpp
+++ Pekarnya/main.cpp
@@ -10,10 +10,12 @@ int main()
     Worker Vadim("Vadim","1234","admin");
     Worker Andrey("Andrey","1234","pekar");
     Worker Ivan("Ivan","1234","worker");
+    Worker Ilya;
     ListWorkers WorkerList("Spisok Rabochich");
     WorkerList.AddElement(Vadim);
     WorkerList.AddElement(Andrey);
     WorkerList.AddElement(Ivan);
+    WorkerList.AddElement(Ilya);
     WorkerList.Control();
     system("pause");
 
