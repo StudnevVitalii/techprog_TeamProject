0a0ac806967eae04058f9cf1ca8b46f976ad1992
redactor <Randomer789@yandex.ru>
2023-12-10 16:47:21 +0300

    добавление входа в систему

--- Pekarnya/main.cpp
+++ Pekarnya/main.cpp
@@ -2,32 +2,14 @@
 #include <string>
 #include "elements.h"
 #include "lists.h"
+#include "userinterface.h"
 
 using namespace std;
 
 int main()
 {
-    Worker Vadim("Vadim","1234","admin");
-    Worker Andrey("Andrey","1234","pekar");
-    Worker Ivan("Ivan","1234","worker");
-    //Worker Ilya;
-    ListWorkers WorkerList("Spisok Rabochich");
-    WorkerList.AddElement(Vadim);
-    WorkerList.AddElement(Andrey);
-    WorkerList.AddElement(Ivan);
-    Worker* W1 = WorkerList.Check("Ivan");
-    if (W1 != nullptr){
-        W1->shou();
-        system("pause");
-    }
-    W1 = WorkerList.Check("1");
-    if (W1 == nullptr){
-        cout << "ok" << endl;
-        system("pause");
-    }
-
-    WorkerList.AddElement(Ilya);
-    WorkerList.Control();
+    UserInterface the_user_interface;
+    the_user_interface.Login();
     system("pause");
 
 
--- Pekarnya/userinterface.cpp
+++ Pekarnya/userinterface.cpp
@@ -6,5 +6,48 @@ UserInterface::UserInterface()
 }
 
 void UserInterface::Login(){
+    Worker Vadim("Vadim","1234","admin");
+    Worker Andrey("Andrey","1234","pekar");
+    Worker Ivan("Ivan","1234","worker");
+    ListWorkers WorkerList("Spisok Rabochich");
+    WorkerList.AddElement(Vadim);
+    WorkerList.AddElement(Andrey);
+    WorkerList.AddElement(Ivan);
 
+
+
+    string login;
+    string password;
+    bool exit = false;
+    do{
+        cout << "Login: ";
+        cin >> login;
+        Worker* buff = WorkerList.Check(login);
+        if(buff == nullptr){
+            cout << "Takogo polzovatelya ne sushchestvuet!!" << endl;
+        }
+        else{
+            cout << "Parol: ";
+            cin >> password;
+            if(buff->GetPassword() == password){
+                switch (buff->GetRoot()[0]) {
+                case 'a':
+                    cout << "admin";
+                    exit = true;
+                    break;
+                case 'p':
+                    cout << "pekar";
+                    exit = true;
+                    break;
+                case 'w':
+                    cout << "worker";
+                    exit = true;
+                    break;
+                }
+            }
+            else{
+                cout << "Vveden nevernyj parol polzovatelya!!" << endl;
+            }
+        }
+    }while(!exit);
 }
